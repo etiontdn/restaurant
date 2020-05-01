c9d40ff38c1b14e21db690f5aa8a2b0f2bf65fef
Etiontdn <etiontdn@gmail.com>
2020-05-01 18:55:24 -0300

    basic layout

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/pages/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: "./dist",
+    watchContentBase: true
+  },
+};
